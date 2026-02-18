0247fb93c0d189c199a4543d9401dc352aa368ab
Divyanshu Srivastava <tanaysrivastav13@gmail.com>
2026-02-18 23:52:26 +0530

    final issues resolved

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -2,11 +2,10 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "Expense_tracker",
-  password: "postgres",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default pool;
